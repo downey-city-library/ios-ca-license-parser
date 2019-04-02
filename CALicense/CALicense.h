994323c95acc0d04625428975bf3b820d9268216
Andrew Despres <21027320+a-despres@users.noreply.github.com>
2019-04-02 13:37:33 -0700

    Add project files
    Add the xcode project files that will serve as the foundation for this project.

---
+++ CALicense/CALicense.h
@@ -0,0 +1,19 @@
+//
+//  CALicense.h
+//  CALicense
+//
+//  Created by Steve Ross on 4/2/19.
+//  Copyright © 2019 Downey City Library. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CALicense.
+FOUNDATION_EXPORT double CALicenseVersionNumber;
+
+//! Project version string for CALicense.
+FOUNDATION_EXPORT const unsigned char CALicenseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CALicense/PublicHeader.h>
+
+
