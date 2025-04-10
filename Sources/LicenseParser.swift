5a0fee1e8feaf4ee0dc1e2c3a367b4fb4eb98684
Andrew Despres <21027320+a-despres@users.noreply.github.com>
2025-04-10 15:58:18 -0700

    Custom init for LicenseField
    Create a custom init for LicenseField to assign code and value from a single input.

--- Sources/LicenseParser.swift
+++ Sources/LicenseParser.swift
@@ -14,18 +14,15 @@ public struct LicenseParser {
         // The license should be the first of the subfiles.
         // All other subfiles are then added without processing to the License.Raw object.
         let fields = subfiles.first?.value.components(separatedBy: header.dataElementSeparator).map {
-            LicenseField(code: LicenseFieldCode(rawValue: String($0.prefix(3))) ?? .unk, value: trimField($0))
+            LicenseField(data: $0)
         } ?? []
         
         var license = License(header: header, data: fields)
+        
         if subfiles.count > 1 {
             license.raw.subfiles = Array(subfiles[1..<subfiles.count])
         }
         
         return license
-    }
-    
-    internal static func trimField(_ field: String) -> String {
-        String(field.dropFirst(3))
     }
 }
--- Sources/Models/LicenseField.swift
+++ Sources/Models/LicenseField.swift
@@ -1,6 +1,11 @@
 import Foundation
 
 struct LicenseField {
+    init(data: String) {
+        code = LicenseFieldCode(rawValue: String(data.prefix(3))) ?? .unk
+        value = String(data.dropFirst(3))
+    }
+    
     let code: LicenseFieldCode
     let value: String
 }
